8085eac22bcc553117efbe07f1ca15b6ea1ea92f
Justin Wilcox <justin@needtobounce.com>
2017-07-09 21:01:46 -0700

    Adding flights skeleton and can't make it button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var options = { videoId: '8HM7XxQGdt0', start: 35 };
-	$('#wrapper').tubular(options);
+    $('#wrapper').tubular(options);
+
+    // KAYAK.embed({
+    //     container: document.getElementById("#kayakSearchWidgetContainer"),
+    //     autoPosition: true,
+    //     defaultProduct: "flights",
+    //     enabledProducts: ["hotels", "flights"],
+    //     startDate: "2018-09-20",
+    //     endDate: "2018-09-23",
+    //     destination: "Kona, HI",
+    //     baseURL: "www.kayak.co.uk",
+    //     ssl: true,
+    //     travelers: 2, 
+    //     isInternalLoad: false
+    // });
 });
