50ae9030157aade166a2ae90772f309ca806b442
Justin Wilcox <justin@needtobounce.com>
2019-12-22 09:24:33 -0800

    xmas push

---
+++ xmas2019/js/main.js
@@ -0,0 +1,33 @@
+"use strict"
+
+$(document).ready(function () {
+    /* height 100% as window height */
+    $('.vh-100').css("min-height", $(window).height());
+    /* height 100% as window height */
+    $('.vhf-100').css("min-height", $(window).height() - $('.footer').outerHeight() );
+    $('#passwordAlert').alert();
+    $('#passwordAlert').hide();
+
+    $('#submitButton').click(function(event) {
+        event.preventDefault();
+        $('#passwordAlert').fadeOut();
+
+        if ($('#exampleInputPassword1').val().trim().toLowerCase() == "escape room") {
+            $('#about').fadeIn();
+            $('#events').fadeIn();
+            document.getElementById("about").scrollIntoView();
+        }
+        else {
+            $('#passwordAlert').fadeIn();
+        }
+    })
+});
+/* Scroll content add class */
+$(window).on("scroll", function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 150) {
+        $(".header").addClass("active")
+    } else {
+        $(".header").removeClass("active")
+    }
+});
